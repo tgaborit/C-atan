f62ce62a144ee80985101f8cace18a03fd06be09
Emmanuel Durel <emmanuel.durel@insa-rennes.fr>
2019-03-26 17:58:45 +0100

    ajout fct deplacement sur le plateau et ses tests

--- code/modele/sources/test_plateau.c
+++ code/modele/sources/test_plateau.c
@@ -34,12 +34,36 @@ static void test_freePlateau_PlateauNULL(void** state){
     assert_int_equal(-1,freePlateau(NULL));
 }
 
+static void test_deplacementPlateau_coordBonnes(void** state){
+    Plateau* p = (Plateau*) (*state);
+    assert_int_equal((int) p,(int)deplacementPlateau(p,0,0));
+    assert_int_equal((int)p->adjacence[4]->adjacence[4],(int)deplacementPlateau(p,1,2));
+    assert_int_equal((int)p->adjacence[1],(int)deplacementPlateau(p,-0.5,-1));
+    assert_int_equal((int)p->adjacence[2]->adjacence[4],(int)deplacementPlateau(p,0,2));
+    assert_int_equal((int)p->adjacence[3]->adjacence[1],(int)deplacementPlateau(p,0,-2));
+    assert_int_equal((int)p->adjacence[0]->adjacence[0],(int)deplacementPlateau(p,-2,0));
+    assert_int_equal((int)p->adjacence[5]->adjacence[5],(int)deplacementPlateau(p,2,0));
+}
+
+static void test_deplacementPlateau_coordFausses(void** state){
+    Plateau* p = (Plateau*) (*state);
+    assert_ptr_equal(NULL,deplacementPlateau(p,-2,-2));
+    assert_ptr_equal(NULL,deplacementPlateau(p,-0.5,-2));
+    assert_ptr_equal(NULL,deplacementPlateau(p,0,0.5));
+}
+
+static void test_deplacementPlateau_PlateauNull(void** state){
+    assert_ptr_equal(NULL,deplacementPlateau(NULL,2,0));
+}
 
 int main_test(void){
     const struct CMUnitTest tests_plateau[] = {
         cmocka_unit_test(test_initPlateau_retourNonNul),
         cmocka_unit_test(test_freePlateau_PlateauNULL),
         cmocka_unit_test(test_freePlateau_PlateauNonNul),
+	cmocka_unit_test(test_deplacementPlateau_coordBonnes),
+        cmocka_unit_test(test_deplacementPlateau_coordFausses),
+        cmocka_unit_test(test_deplacementPlateau_PlateauNull)
     };
     return cmocka_run_group_tests(tests_plateau,group_setup,group_teardown);
 }
