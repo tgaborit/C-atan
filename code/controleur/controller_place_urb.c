15a2ddde9212b0e23d4e09c766660b7d2ed66ee7
Titouan Gaborit <tgaborit@insa-rennes.fr>
2019-04-01 23:06:58 +0200

    création fichiers controller_place_urb

---
+++ code/controleur/controller_place_urb.c
@@ -0,0 +1,10 @@
+/**
+*
+*
+* \author Titouan Gaborit
+* \date 1 avril 2019
+*
+*
+*/
+
+#include "controller_place_urb.h"
---
+++ code/controleur/controller_place_urb.h
@@ -0,0 +1,15 @@
+/**
+*
+*
+* \author Titouan Gaborit
+* \date 1 avril 2019
+*
+*
+*/
+
+#ifndef CATANE_CONTROLLER_PLACE_URB_H
+#define CATANE_CONTROLLER_PLACE_URB_H
+
+typedef enum {SETTLE_PLACING, CITY_PLACING} UrbPlacing;
+
+#endif //CATANE_CONTROLLER_PLACE_URB_H
