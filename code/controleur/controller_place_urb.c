6be4648273c21e96d6d0d43d80b0650f07ac9442
Titouan Gaborit <tgaborit@insa-rennes.fr>
2019-04-02 12:47:19 +0200

    ajout squelette fonction controllerPlaceUrb avec événement quit

--- code/controleur/controller_place_urb.c
+++ code/controleur/controller_place_urb.c
@@ -7,4 +7,56 @@
 *
 */
 
+#include <SDL.h>
 #include "controller_place_urb.h"
+
+void controllerPlaceUrb(SDL_bool program_launched, SDL_Renderer* renderer/*, Game* the_game*/)
+{
+    SDL_bool placing_launched = SDL_TRUE;
+    while(program_launched && placing_launched)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+            case SDL_KEYDOWN :
+                switch(event.key.keysym.sym)
+                {
+                case SDLK_q :
+                    printf("Appui sur touche Q\n");
+                    printf("Appel de la fonction quit(&program_launched)\n");
+                    quit(&program_launched);
+                    break;
+
+                default :
+                    break;
+                }
+                break;
+
+            case SDL_QUIT :
+                printf("Evenement SDL_QUIT\n");
+                printf("Appel de la fonction quit(&program_launched)\n");
+                quit(&program_launched);
+                break;
+
+            default :
+                break;
+            }
+        }
+    }
+}
+
+/**
+* \fn void quit(SDL_bool * program_launched)
+* \brief Fonction activant la fin du jeu
+*
+* Donne la valeur SDL_False à l'état du programme, ce qui le fera sortir de la boucle de controlerTurn et quitter du programme
+*
+* \param[in,out] pprogram_launched Pointeur vers l'état du programme
+*
+*/
+void quit(SDL_bool * pprogram_launched)
+{
+    *pprogram_launched = SDL_FALSE;
+}
--- code/controleur/controller_place_urb.h
+++ code/controleur/controller_place_urb.h
@@ -12,4 +12,8 @@
 
 typedef enum {SETTLE_PLACING, CITY_PLACING} UrbPlacing;
 
+void controllerPlaceUrb(SDL_bool program_launched, SDL_Renderer* renderer/*, Game* the_game*/);
+
+void quit(SDL_bool * pprogram_launched);
+
 #endif //CATANE_CONTROLLER_PLACE_URB_H
