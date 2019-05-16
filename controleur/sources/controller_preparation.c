493e4a4e37bf3fd35d9cf0334597a9e3d4671b5f
Titouan Gaborit <tgaborit@insa-rennes.fr>
2019-05-16 15:14:19 +0200

    ajout gestion deuxièmes colonies dans préparation

--- controleur/sources/controller_preparation.c
+++ controleur/sources/controller_preparation.c
@@ -4,32 +4,36 @@
 #include "controller_crossing.h"
 #include "converters.h"
 #include "set_plateau.h"
+#include "set_partie.h"
 
 
 void controllerPreparation(SDL_bool* program_launched, SDL_Window* window, Partie* the_game)
 {
-    int j, bool_colonie;
+    int i, j, bool_colonie;
     updateFenetre(the_game, window);
-    for(j = 0; j < get_nbjoueurs(the_game); ++j)
+    for(i = 0; i < 2; ++i)
     {
-        CrossButton cross_clicked;
-        CrossCoordinates cross_chosen;
-
-        do
+        for(j = 0; j < get_nbjoueurs(the_game); ++j)
         {
-            cross_clicked = NO_CROSSBUTTON;
-            controllerCrossing(&cross_clicked, window, program_launched);
-            if(*program_launched == SDL_FALSE){
-                return;
-            }
+            CrossButton cross_clicked;
+            CrossCoordinates cross_chosen;
 
-            cross_chosen = crossButtonToCrossCoordinates(cross_clicked);
-            printf("Appel de la fonction du modele setColonie(the_game, %f, %f, %d)\n", cross_chosen.x, cross_chosen.y, cross_chosen.position);
-            bool_colonie = setColonieInit(the_game, cross_chosen.x, cross_chosen.y, cross_chosen.position);
+            do
+            {
+                cross_clicked = NO_CROSSBUTTON;
+                controllerCrossing(&cross_clicked, window, program_launched);
+                if(*program_launched == SDL_FALSE){
+                    return;
+                }
+
+                cross_chosen = crossButtonToCrossCoordinates(cross_clicked);
+                printf("Appel de la fonction du modele setColonie(the_game, %f, %f, %d)\n", cross_chosen.x, cross_chosen.y, cross_chosen.position);
+                bool_colonie = setColonieInit(the_game, cross_chosen.x, cross_chosen.y, cross_chosen.position);
+            }
+            while (bool_colonie != 0);
+            passer_tour(the_game);
+            updateFenetre(the_game, window);
         }
-        while (bool_colonie != 0);
-        passer_tour(the_game);
-        updateFenetre(the_game, window);
     }
 }
 
