51a8e0e48c1908a53f77772faf566d7f9660f7ee
Titouan Gaborit <tgaborit@insa-rennes.fr>
2019-05-16 15:22:04 +0200

    ajout gestion des routes dans préparation

--- controleur/sources/controller_preparation.c
+++ controleur/sources/controller_preparation.c
@@ -9,7 +9,7 @@
 
 void controllerPreparation(SDL_bool* program_launched, SDL_Window* window, Partie* the_game)
 {
-    int i, j, bool_colonie;
+    int i, j, bool_colonie, bool_route;
     updateFenetre(the_game, window);
     for(i = 0; i < 2; ++i)
     {
@@ -31,6 +31,27 @@ void controllerPreparation(SDL_bool* program_launched, SDL_Window* window, Parti
                 bool_colonie = setColonieInit(the_game, cross_chosen.x, cross_chosen.y, cross_chosen.position);
             }
             while (bool_colonie != 0);
+            updateFenetre(the_game, window);
+
+
+            PathButton path_clicked;
+            PathCoordinates path_chosen;
+
+            do
+            {
+                path_clicked = NO_PATHBUTTON;
+                controllerPath(&path_clicked, window, program_launched);
+                if(*program_launched == SDL_FALSE){
+                    return;
+                }
+
+                path_chosen = pathButtonToPathCoordinates(path_clicked);
+                printf("Appel de la fonction du modele setRouteFree(the_game, %f, %f, %d)\n", path_chosen.x, path_chosen.y, path_chosen.position);
+                bool_route = setRouteFree(the_game, path_chosen.x, path_chosen.y, path_chosen.position);
+            }
+            while (bool_route != 0);
+
+
             passer_tour(the_game);
             updateFenetre(the_game, window);
         }
